0686ad363ac0ee765809e95bc2b6774789f112bb
Vince Buongiovanni <vincentbuongiovanni@gmail.com>
2022-09-09 16:10:35 -0600

    finished Global Stats component along with initial styling

--- client/src/components/user-statistics-components/UserStats.js
+++ client/src/components/user-statistics-components/UserStats.js
@@ -1,31 +1,70 @@
 import {useState, useEffect, useContext} from "react";
 import axios from "axios";
+import {useNavigate} from "react-router-dom";
 import {UserContext} from "../UserContext";
 import Header from "../Header";
 import NavBar from '../NavBar';
+import UserStatsTabset from "./UserStatsTabset";
+import UserStatsPane from "./UserStatsPane";
+
 const UserStats = () => {
 
-  // load and deconstruct context:
-  const {credentials} = useContext(UserContext);
-  
-  useEffect(() => {
-    const requestBody = {
-      userName : credentials.userName,
-      password : credentials.password,
-    }
-    axios.post("/user/global/" + credentials._id, requestBody)
-      .then(res => {
-        console.log(res.data)
-      })
-      .catch(err => console.log(err))
+  // create nav object
+  const navigate = useNavigate();
 
-  }, [])
+  // load and deconstruct context:
+    const {credentials} = useContext(UserContext);
+
+  // create state to contain global statistics data
+    const [globalStats, setGlobalStats] = useState({
+      summarizedSubjects: [],
+      summarizedQuizzes : [],
+      summarizedTopics : []
+    })
+    const [tabSelection, setTabSelection] = useState("Subjects")
+
+    const selections = ["Subjects", "Quizzes", "Topics"]
+  
+  // wrap http request in useEffect - single render
+    useEffect(() => {
+      const requestBody = {
+        userName : credentials.userName,
+        password : credentials.password,
+      }
+      axios.post("/user/global/" + credentials._id, requestBody)
+        .then(res => {
+          setGlobalStats(res.data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+  // handle nav to main menu
+    const navToMain = () => {
+      navigate("/menu/");
+    }
+
+  // handle change between tabset
+    const changeTabset = e => {
+      const {id} = e.target;
+      setTabSelection(id);
+    }
+
+  // consolidate props
+  const tabsetProps = {tabSelection, changeTabset, selections}
 
   return (
     <main>
       <Header negateMetrics={true}/>
       <NavBar />
-      UserStats!
+      <div className="userStatsContainer">
+        <div className="userStatsContent">
+            <UserStatsTabset tabsetProps={tabsetProps}/>
+            <UserStatsPane globalStats={globalStats} tabSelection={tabSelection}/>
+          <div className="btnContainer btnContainerSingle">
+            <button className="colorBtn" onClick={navToMain}>Back</button>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
